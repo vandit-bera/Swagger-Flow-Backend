d5c7b3b06c6a667afa853d1fb8efc2792666717f
vandit-bera <vanditbera@gmail.com>
2023-04-24 22:01:23 +0530

    add /course route in path

--- index.js
+++ index.js
@@ -11,8 +11,30 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const PORT = 4000
 
+let course = [
+    {
+        id: "1",
+        name: "ReactJS",
+        price: 299
+    },
+    {
+        id: "2",
+        name: "SwaggerAPI",
+        price: 399
+    },
+    {
+        id: "3",
+        name: "AngulaJS",
+        price: 499
+    }
+]
+
 app.get("/", (req, res) => {
     res.status(200).send("Hello, Vandit")
+})
+
+app.get("/api/v1/course", (req, res) => {
+    res.status(200).send("Hello from /api/v1/course")
 })
 
 app.listen(PORT, () => {
